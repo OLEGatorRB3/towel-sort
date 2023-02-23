e8f453027a795ebaeae9edce6e197cc1cfb77c74
OLEGatorRB3 <olegatorrb3@gmail.com>
2023-02-23 19:51:36 +0300

    fit: done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 
-// You should implement your task here.
+function alone(arr){
+    let arr2 = [];
+    for(let i = 0; i < arr.length; i++){
+        if(i % 2) {
+            arr2[i] = arr[i].sort((a, b) => b - a)
+        }
+        arr2[i] = arr[i]     
+    }
+    let arr3 = arr2.reduce((acc, cur) => {
+        return acc.concat(cur)
+    }, [])
+    return arr3
+}
 
 module.exports = function towelSort (matrix) {
   return [];
